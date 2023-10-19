51f1e23ef3751341d476781842b20c3d9bf53297
Mehwish <mehwishgul715@gmail.com>
2023-10-19 14:33:46 +0500

    commit

---
+++ assets/Js/register.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    var current_fs, next_fs, previous_fs;
+    var animating;
+  
+    $(".next").click(function () {
+      if (animating) return false;
+      animating = true;
+      current_fs = $(this).parent();
+      next_fs = $(this).parent().next();
+      $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+      next_fs.show();
+      current_fs.hide();
+      animating = false;
+    });
+  
+    $(".submit").click(function () {
+      if (animating) return false;
+      animating = true;
+      current_fs = $(this).parent();
+      next_fs = $(this).parent().next();
+      $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+      next_fs.show();
+      current_fs.hide();
+      animating = false;
+    });
+  
+    $(".previous").click(function () {
+      if (animating) return false;
+      animating = true;
+      current_fs = $(this).parent();
+      previous_fs = $(this).parent().prev();
+      $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+      previous_fs.show();
+      current_fs.hide();
+      animating = false;
+    });
+  });
+  
