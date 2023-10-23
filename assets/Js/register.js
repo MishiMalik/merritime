6d83980a0a4f5002882e082e4b97d3a81bd76989
Mehwish <mehwishgul715@gmail.com>
2023-10-23 19:28:41 +0500

    commit

--- assets/Js/register.js
+++ assets/Js/register.js
@@ -1,56 +1,77 @@
 $(document).ready(function () {
-  var current_fs, next_fs, previous_fs;
-  var animating;
-
-  function validateStepFields(step) {
+    var current_fs, next_fs, previous_fs;
+    var animating;
+  
+    function validateStepFields(step) {
       var requiredFields = step.find('[required]');
       var isValid = true;
-
+  
       for (var i = 0; i < requiredFields.length; i++) {
-          if (!requiredFields[i].checkValidity()) {
-              isValid = false;
-              $(requiredFields[i]).css('background-color', 'pink'); // Change background color
-          } else {
-              $(requiredFields[i]).css('background-color', ''); // Reset background color
-          }
+        if (!requiredFields[i].checkValidity()) {
+          isValid = false;
+          $(requiredFields[i]).css('background-color', 'pink'); // Change background color
+        } else {
+          $(requiredFields[i]).css('background-color', ''); // Reset background color
+        }
       }
-
+  
       return isValid;
-  }
+    }
+  
+    $("#progressbar li").click(function () {
+        if (animating) return false;
+        if (!$(this).hasClass("active")) return false; // Check for the "active" class
+        animating = true;
+    
+        var stepIndex = $(this).index(); // Get the index of the clicked li
+        current_fs = $("fieldset:visible");
+        next_fs = $("fieldset:eq(" + stepIndex + ")");
+    
+        if (stepIndex < $("fieldset").index(current_fs)) {
+          current_fs.hide();
+          next_fs.show();
+        } else if (stepIndex > $("fieldset").index(current_fs)) {
+          next_fs.show();
+          current_fs.hide();
+        }
+    
+        animating = false;
+      });
 
-  $(".next").click(function () {
+
+    $(".next").click(function () {
       if (animating) return false;
       animating = true;
       current_fs = $(this).closest("fieldset");
       next_fs = current_fs.next();
-
+  
       if (!validateStepFields(current_fs)) {
-          animating = false;
-          return;
+        animating = false;
+        return;
       }
-
+  
       $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
       next_fs.show();
       current_fs.hide();
       animating = false;
-  });
-
-  $(".submit").click(function () {
+    });
+  
+    $(".submit").click(function () {
       if (animating) return false;
       animating = true;
-      current_fs = $(this).closest("fieldset");
+      current_fs = $("fieldset:visible");
       next_fs = current_fs.next();
       if (!validateStepFields(current_fs)) {
-          animating = false;
-          return;
+        animating = false;
+        return;
       }
       $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
       next_fs.show();
       current_fs.hide();
       animating = false;
-  });
-
-  $(".previous").click(function () {
+    });
+  
+    $(".previous").click(function () {
       if (animating) return false;
       animating = true;
       current_fs = $(this).closest("fieldset");
@@ -59,5 +80,6 @@ $(document).ready(function () {
       previous_fs.show();
       current_fs.hide();
       animating = false;
+    });
   });
-});
+  
